63a7d5fff01240bfce750880c1638c14ca0fd9b6
Fab Sto <fabien.stodel@laposte.net>
2024-03-19 15:04:12 +0100

    delete duplicate from request and sorted by rating

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/search", async (req, res) => {
   const response2 = await axios.post(
     `https://places.googleapis.com/v1/places:searchText?key=${process.env.GOOGLE_API_KEY}`,
     {
-      textQuery: "eglise ancienne",
+      textQuery: "restaurant",
       // includedType: "visitor_center",
       strictTypeFiltering: true,
       locationBias: {
@@ -33,7 +33,7 @@ app.get("/search", async (req, res) => {
           radius: 5000,
         },
       },
-      minRating: 3,
+      minRating: 4,
     },
     {
       headers: {
@@ -45,7 +45,8 @@ app.get("/search", async (req, res) => {
           "places.regularOpeningHours.weekdayDescriptions",
           "places.primaryType",
           "places.shortFormattedAddress",
-          "places.photos",
+          "places.id",
+          // "places.photos",
         ],
       },
     }
@@ -56,10 +57,98 @@ app.get("/search", async (req, res) => {
   //   ?key=${process.env.GOOGLE_API_KEY}&maxHeightPx=1000&maxWidthPx=1000&skipHttpRedirect=true`
   // );
 
+  const response3 = await axios.post(
+    `https://places.googleapis.com/v1/places:searchText?key=${process.env.GOOGLE_API_KEY}`,
+    {
+      textQuery: "restaurant",
+      // includedType: "visitor_center",
+      strictTypeFiltering: true,
+      locationBias: {
+        circle: {
+          center: {
+            latitude: lat,
+            longitude: long,
+          },
+          radius: 5000,
+        },
+      },
+      minRating: 4,
+    },
+    {
+      headers: {
+        "X-Goog-FieldMask": [
+          // "*",
+          "places.displayName",
+          "places.rating",
+          "places.websiteUri",
+          "places.regularOpeningHours.weekdayDescriptions",
+          "places.primaryType",
+          "places.shortFormattedAddress",
+          "places.id",
+          // "places.photos",
+        ],
+      },
+    }
+  );
+
+  const response4 = await axios.post(
+    `https://places.googleapis.com/v1/places:searchText?key=${process.env.GOOGLE_API_KEY}`,
+    {
+      textQuery: "restaurant",
+      // includedType: "visitor_center",
+      strictTypeFiltering: true,
+      locationBias: {
+        circle: {
+          center: {
+            latitude: lat,
+            longitude: long,
+          },
+          radius: 5000,
+        },
+      },
+      minRating: 4,
+    },
+    {
+      headers: {
+        "X-Goog-FieldMask": [
+          // "*",
+          "places.displayName",
+          "places.rating",
+          "places.websiteUri",
+          "places.regularOpeningHours.weekdayDescriptions",
+          "places.primaryType",
+          "places.shortFormattedAddress",
+          "places.id",
+          // "places.photos",
+        ],
+      },
+    }
+  );
+
+  const resp2 = response2.data.places;
+  const resp3 = response3.data.places;
+  const resp4 = response4.data.places;
+
+  let responseTotal = resp2.concat(resp3, resp4);
+
+  function removeDuplicates() {
+    jsonObject = responseTotal.map(JSON.stringify);
+    uniqueSet = new Set(jsonObject);
+    responseTried = Array.from(uniqueSet).map(JSON.parse);
+  }
+  removeDuplicates();
+
+  console.log(responseTried);
+
+  function compareByRate(a, b) {
+    return b.rating - a.rating;
+  }
+
+  responseTried = responseTried.sort(compareByRate);
+
   res.status(200).json({
-    count: response2.data.places.length,
-    data: response2.data,
-    // photo: photo,
+    count: responseTried.length,
+    responseTried: responseTried,
   });
 });
 
